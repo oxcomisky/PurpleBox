7400750d4845679bf846ba7b959973753e0c1fe5
oxcomisky <49965713+oxcomisky@users.noreply.github.com>
2019-09-26 12:32:45 -0500

    Add files via upload

---
+++ InterfaceAdmin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package purplebox;
+
+/**
+ *
+ * @author oxcom
+ */
+public interface InterfaceAdmin {
+    public void addMovie();
+    public void removeMovie();
+    public void changePriceDVD();
+    public void changePriceBluRay();
+    public void changePriceGame();
+    public void volumeDiscount();
+    public void addPromoCode();
+    public void disableUnit();
+    public void EnableUnit();
+}
---
+++ InterfaceUser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package purplebox;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author oxcom
+ */
+public interface InterfaceUser {
+    
+    public ArrayList search();
+    public void returnDisc();
+    public int promoCode();
+    public ArrayList pay();
+    public void remove();
+    public void removeAll();
+}
