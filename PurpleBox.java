a3ac402ee67e2c56ec0109e0f521d2754c1f5968
oxcomisky <49965713+oxcomisky@users.noreply.github.com>
2019-10-02 15:16:32 -0500

    Add files via upload
    The skeleton class shouldve been purplebox and that class should implement all necessary Interfaces

--- PurpleBox.java
+++ PurpleBox.java
@@ -5,17 +5,224 @@
  */
 package purplebox;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author oxcom
+ * @author Jon Comisky
  */
-public class PurpleBox {
+public class PurpleBox implements InterfaceAdmin, InterfaceUser, InterfaceDisc, InterfacePromoCode{
+    
+    //Disc Data Members
+    String title;
+    String genre;
+    String type;
+    String release;
+    int criticScore;
+    int quantity;
+    double price;
+    //end Disc Dta members
+    
+    //PromoCode Data Members
+    int code;
+    double percentOff;
+    int codeType;
+    //end PromoCode Data Members
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public PurpleBox() {
+    }
+
+    public PurpleBox(String title, String genre, String type, String release, int criticScore, int quantity, double price) {
+        this.title = title;
+        this.genre = genre;
+        this.type = type;
+        this.release = release;
+        this.criticScore = criticScore;
+        this.quantity = quantity;
+        this.price = price;
     }
     
+    
+    
+    public PurpleBox(String title, String genre, String type, String release, int criticScore, int quantity, double price, int code, double percentOff, int codeType) {
+        this.title = title;
+        this.genre = genre;
+        this.type = type;
+        this.release = release;
+        this.criticScore = criticScore;
+        this.quantity = quantity;
+        this.price = price;
+        this.code = code;
+        this.percentOff = percentOff;
+        this.codeType = codeType;
+    }
+    
+    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+
+    public int getCriticScore() {
+        return criticScore;
+    }
+
+    public void setCriticScore(int criticScore) {
+        this.criticScore = criticScore;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public double getPercentOff() {
+        return percentOff;
+    }
+
+    public void setPercentOff(double percentOff) {
+        this.percentOff = percentOff;
+    }
+
+    public int getCodeType() {
+        return codeType;
+    }
+
+    public void setCodeType(int codeType) {
+        this.codeType = codeType;
+    }
+    
+    
+    
+    
+    //admin methods
+     @Override
+    public void addDisc(Disc disc, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeDisc(Disc disc, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changePriceDVD(double newPrice, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changePriceBluRay(double newPrice, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changePriceGame(double newPrice, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void volumeDiscount(int cartTotal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addPromoCode(int code, ArrayList PromoCodeList, double percentOff) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void disableUnit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void EnableUnit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    //end admin methods 
+    
+    //user methods
+    @Override
+    public ArrayList search(String key, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void returnDisc(Disc disc, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int promoCode(int code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList pay(int cardNumber, ArrayList shoppingCart) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Disc disc, ArrayList shoppingCart) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeAll(ArrayList shoppingCart) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean available(Disc disc, ArrayList inventory) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    //end user methods
 }
